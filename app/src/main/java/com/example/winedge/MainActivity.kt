87d2a81e9f188412940072b596cf4b41c3a11641
trust-layer <115367286+Egolin77@users.noreply.github.com>
2026-07-03 15:10:00 +0200

    Change start URL from Bing to Google Apps Script

--- app/src/main/java/com/example/winedge/MainActivity.kt
+++ app/src/main/java/com/example/winedge/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     private var fileUploadCallback: ValueCallback<Array<Uri>>? = null
     private var pendingPermissionRequest: PermissionRequest? = null
 
-    private val startUrl = "https://www.bing.com"
+    private val startUrl = "https://script.google.com/macros/s/AKfycbwoytNsui-y7jXl8xDGAe000MSRriz9vhaVqnPFibY/dev"
 
     private val fileChooserLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
