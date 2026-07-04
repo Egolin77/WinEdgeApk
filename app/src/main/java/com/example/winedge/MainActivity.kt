360c6a6ec1517ead57f1f87c768474d09d05d923
trust-layer <115367286+Egolin77@users.noreply.github.com>
2026-07-04 16:14:31 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/winedge/MainActivity.kt
+++ app/src/main/java/com/example/winedge/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     private var fileUploadCallback: ValueCallback<Array<Uri>>? = null
     private var pendingPermissionRequest: PermissionRequest? = null
 
-    private val startUrl = "https://bing.com"
+    private val startUrl = "https://outlook.cloud.microsoft/mail"
 
     private val fileChooserLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
