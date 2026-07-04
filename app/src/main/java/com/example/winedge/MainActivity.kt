e78b0cc072cd9fcbd614c1510b1f4d4023a3c76d
trust-layer <115367286+Egolin77@users.noreply.github.com>
2026-07-04 04:59:27 +0200

    Change start URL for MainActivity

--- app/src/main/java/com/example/winedge/MainActivity.kt
+++ app/src/main/java/com/example/winedge/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     private var fileUploadCallback: ValueCallback<Array<Uri>>? = null
     private var pendingPermissionRequest: PermissionRequest? = null
 
-    private val startUrl = "https://admin.cloud.microsoft/?source=applauncher#/alladmincenters"
+    private val startUrl = "https://nilfisk.talkdeskid.eu/login"
 
     private val fileChooserLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
