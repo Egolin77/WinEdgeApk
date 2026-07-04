336689723d502dc6469762caf89323d381ca398f
trust-layer <115367286+Egolin77@users.noreply.github.com>
2026-07-04 11:30:21 +0200

    Change start URL from Facebook to Bing

--- app/src/main/java/com/example/winedge/MainActivity.kt
+++ app/src/main/java/com/example/winedge/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     private var fileUploadCallback: ValueCallback<Array<Uri>>? = null
     private var pendingPermissionRequest: PermissionRequest? = null
 
-    private val startUrl = "https://www.facebook.com"
+    private val startUrl = "https://www.bing.com"
 
     private val fileChooserLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
